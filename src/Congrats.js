479fdfa4e0fac1a9739f25099c65122bae1293ed
Daniel Villarreal <dvillarreal93@gmail.com>
2022-01-07 13:17:47 -0700

    Tests green for Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -4,6 +4,16 @@
  * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if `sucess` prop is false)
  */
-export default function Congrats({ success }) {
-  return <div />;
+export default function Congrats(props) {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratluations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 }
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -20,7 +20,7 @@ const setup = (props = {}) => {
 test('renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-congrats');
-  expect(component.lengtg).toBe(1);
+  expect(component.length).toBe(1);
 });
 test('renders no text when `success` prop is false', () => {
   const wrapper = setup({ success: false });

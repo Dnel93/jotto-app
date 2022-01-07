eeac2af7762e1eef3a19227e8f221a4cd3540104
Daniel Villarreal <dvillarreal93@gmail.com>
2022-01-07 13:13:11 -0700

    red flags on texts

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/await-async-query */
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Congrats from './Congrats';
@@ -16,6 +17,18 @@ const setup = (props = {}) => {
   return shallow(<Congrats {...props} />);
 };
 
-test('renders without error', () => {});
-test('renders no text when `success` prop is false', () => {});
-test('renders non-empty congrats message when `success` prop is true', () => {});
+test('renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.lengtg).toBe(1);
+});
+test('renders no text when `success` prop is false', () => {
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.text()).toBe('');
+});
+test('renders non-empty congrats message when `success` prop is true', () => {
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, 'congrats-message');
+  expect(message.text().length).not.toBe('congrats');
+});

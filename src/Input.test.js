6fc3ca9429d39bf007327cb67bc513e819fddf59
Daniel Villarreal <dvillarreal93@gmail.com>
2022-01-11 13:35:00 -0700

    ADded basic test for Input component

--- src/Input.test.js
+++ src/Input.test.js
@@ -3,18 +3,26 @@ import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
-const defaultProps = {};
+const defaultProps = {
+    secretWord: ""
+};
 
 /**
  * Setup function for app component
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const setup = (props = {}) => {
+    const setupProps = { ...defaultProps, ...props}
+  return shallow(<Input {...setupProps} />);
 };
 
 test('renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+test('does noo throw warning with expected props', () => {
+    const expectedProps = { secretWord: ""};
+    checkProps(Input, expectedProps);
 });

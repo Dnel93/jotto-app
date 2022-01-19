09bdd1aec8e56f4d0abe53058536157873b41091
Daniel Villarreal <dvillarreal93@gmail.com>
2022-01-19 15:16:54 -0700

    Added test with useState

--- src/Input.test.js
+++ src/Input.test.js
@@ -4,7 +4,7 @@ import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
 const defaultProps = {
-    secretWord: ""
+  secretWord: '',
 };
 
 /**
@@ -12,7 +12,7 @@ const defaultProps = {
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
-    const setupProps = { ...defaultProps, ...props}
+  const setupProps = { ...defaultProps, ...props };
   return shallow(<Input {...setupProps} />);
 };
 
@@ -23,6 +23,21 @@ test('renders without error', () => {
 });
 
 test('does noo throw warning with expected props', () => {
-    const expectedProps = { secretWord: ""};
-    checkProps(Input, expectedProps);
+  const expectedProps = { secretWord: '' };
+  checkProps(Input, expectedProps);
+});
+
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
 });

876b984b184a646e357a30d90f34739492243ea4
Daniel Villarreal <dvillarreal93@gmail.com>
2022-01-19 15:39:36 -0700

    Clearing state-controlled field on submit

--- src/Input.test.js
+++ src/Input.test.js
@@ -3,6 +3,13 @@ import { shallow } from 'enzyme';
 import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
 
+// mock entire module for destructuring useState on import
+// let mockSetCurrentGuess = jest.fn();
+// jest.mock('react', () => ({
+//   ...jest.requireActual('react'),
+//   useState: initialState => [initialState, mockSetCurrentGuess],
+// }));
+
 const defaultProps = {
   secretWord: '',
 };
@@ -28,16 +35,31 @@ test('does noo throw warning with expected props', () => {
 });
 
 describe('state controlled input field', () => {
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  let originalUseState;
+
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    originalUseState = React.useState;
+    React.useState = () => ['', mockSetCurrentGuess];
+    wrapper = setup();
+  });
+
+  afterEach(() => {
+    React.useState = originalUseState;
+  });
+
   test('state updates with value of input box upon change', () => {
-    const mockSetCurrentGuess = jest.fn();
-    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
-
-    const wrapper = setup();
     const inputBox = findByTestAttr(wrapper, 'input-box');
-
     const mockEvent = { target: { value: 'train' } };
     inputBox.simulate('change', mockEvent);
-
     expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+
+  test('field is cleared upon submit button click', () => {
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+    submitButton.simulate('click', { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
   });
 });

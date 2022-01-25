78bcc357fec8188a688037e724f31421d2cb48b0
Daniel Villarreal <dvillarreal93@gmail.com>
2022-01-25 12:26:53 -0700

    Added code to handle state properly in  and

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import './App.css';
+
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
 import Input from './Input';
 import { getSecretWord } from './actions';
 
 function App() {
-  // TODO: get props from shared state
-  const success = false;
+  const success = useSelector(state => state.success);
+  const guessedWords = useSelector(state => state.guessedWords);
   const secretWord = 'party';
-  const guessedWords = [];
 
   useEffect(() => {
     getSecretWord();
--- src/guessWord.test.js
+++ src/guessWord.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
 
 import App from './App';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, storeFactory } from '../test/testUtils';
+
+// activate global mock to make sure getSecretWord doesn't make network call
+jest.mock('./actions');
 
 /**
  * Create wrapper with specified initial conditions,
@@ -12,9 +16,13 @@ import { findByTestAttr } from '../test/testUtils';
  * @param {object} state - Initial conditions.
  * @returns {Wrapper} - Enzyme wrapper of mounted App component
  */
-const setup = (state = {}) => {
-  // TODO: apply state
-  const wrapper = mount(<App />);
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = mount(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 
   // add value to input box
   const inputBox = findByTestAttr(wrapper, 'input-box');
@@ -26,7 +34,7 @@ const setup = (state = {}) => {
   return wrapper;
 };
 
-describe.skip('no words guessed', () => {
+describe('no words guessed', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = setup({
@@ -41,23 +49,22 @@ describe.skip('no words guessed', () => {
     expect(guessedWordRows).toHaveLength(1);
   });
 });
-describe.skip('some words guessed', () => {
+describe('some words guessed', () => {
   let wrapper;
-
   beforeEach(() => {
-    wrapper = setup({
-      secretWord: 'party',
-      success: false,
-      guessedWords: [{ guessedWord: 'yes', letterMatchcount: 1 }],
-    });
+  	wrapper = setup({
+    	secretWord: 'party',
+    	success: false,
+    	guessedWords: [{ guessedWord: 'agile', letterMatchCount: 1 }],
+  	});
   });
-
-  test('creates GuessedWords table with multiple rows', () => {
-    const guessedWordRows = findByTestAttr(wrapper, 'guessed-words');
-    expect(guessedWordRows).toHaveLength(2);
+  test('adds row to guessedWords table', () => {
+    const guessedWordNodes = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNodes).toHaveLength(2);
   });
 });
-describe.skip('guess secret word', () => {
+
+describe('guess secret word', () => {
   let wrapper;
 
   beforeEach(() => {

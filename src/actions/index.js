7dde73470585393321416ce82d222ac203053390
Daniel Villarreal <dvillarreal93@gmail.com>
2022-01-25 12:03:40 -0700

    Add Thunk for  piece of state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,36 @@
+import { getLetterMatchCount } from '../helpers';
 import axios from 'axios';
 
 export const actionTypes = {
   CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD',
 };
 
 /**
- * @function correctGuess
- * @returns {object} - Action object with type `CORRECT_GUESS`
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ *  and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {String} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function.
  */
-export function correctGuess() {
-  return {type: actionTypes.CORRECT_GUESS};
-}
+export const guessWord = guessedWord => {
+  return function (dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: {
+        guessedWord,
+        letterMatchCount,
+      },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
+  };
+};
 
 export const getSecretWord = () => {
   // TODO: write actual action in redux / context section

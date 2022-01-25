7d41e41daa215771af1d342dcee31e0f5e433c2b
Daniel Villarreal <dvillarreal93@gmail.com>
2022-01-24 21:00:35 -0700

    Add CORRECT_GUESS action and tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
 
+export const actionTypes = {
+  CORRECT_GUESS: 'CORRECT_GUESS',
+};
+
+/**
+ * @function correctGuess
+ * @returns {object} - Action object with type `CORRECT_GUESS`
+ */
+export function correctGuess() {
+  return {type: actionTypes.CORRECT_GUESS};
+}
+
 export const getSecretWord = () => {
-    // TODO: write actual action in redux / context section
+  // TODO: write actual action in redux / context section
   return axios.get('http://localhost:3030').then(response => response.data);
 };
